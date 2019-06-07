1d79a01592cb1c64c31292ba0c92a00d14b257b5
Rahul Syal <syal2512@gmail.com>
2019-06-07 23:36:09 +0530

    destructuring in components

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,22 +1,23 @@
-import React, {Component} from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React from "react";
+import { View, StyleSheet, TextInput } from "react-native";
 
-export default class Input extends Component {
-    render() {
-        return (
-            <View style={styles.inputText}>
-                <TextInput 
-                value={this.props.value}
-                placeholder="Awesome text here"
-                onChangeText={this.props.changeName} />
-            </View>
-        )
-    }
-}
 const styles = StyleSheet.create({
-    inputText: {
-        borderBottomWidth: 2,
-        borderBottomColor: "black",
-        width: "70%",
-    },
-})
+  inputText: {
+    borderBottomWidth: 2,
+    borderBottomColor: "black",
+    width: "70%"
+  }
+});
+
+const Input = () => {
+  return (
+    <View style={styles.inputText}>
+      <TextInput
+        value={this.props.value}
+        placeholder="Awesome text here"
+        onChangeText={this.props.changeName}
+      />
+    </View>
+  );
+};
+export default Input;
